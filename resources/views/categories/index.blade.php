fd2705fca4fae67cc8c07c76980ebffc37f02b18
andaloussi-dev <neon.andaloussi@gmail.com>
2019-12-14 12:34:53 +0100

    adding the project to github

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+<div class="d-flex justify-content-end">
+        <a href="" class="btn btn-success float-right"> Add categorie</a>
+
+</div>
+   <div class="card card-default">
+       <div class="card-header">
+           Categories
+       </div>
+   </div>
+@endsection
