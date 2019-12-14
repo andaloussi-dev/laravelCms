8cc2b0670a488020aea73a3678bdb5191d624582
andaloussi-dev <neon.andaloussi@gmail.com>
2019-12-14 12:43:30 +0100

    adding s

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="d-flex justify-content-end">
-        <a href="" class="btn btn-success float-right"> Add categorie</a>
-
+        <a href="" class="btn btn-success float-right"> Add categories</a>
 </div>
    <div class="card card-default">
        <div class="card-header">
