f153656c8ee68af10d7ec44bfb56b0a74385612f
andaloussi-dev <neon.andaloussi@gmail.com>
2019-12-14 12:52:19 +0100

    adding another s

--- database/migrations/2019_12_14_082913_create_posts_table.php
+++ database/migrations/2019_12_14_082913_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->text('body');
+            $table->string('image');
             $table->timestamps();
         });
     }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="d-flex justify-content-end">
-        <a href="" class="btn btn-success float-right"> Add categories</a>
+        <a href="" class="btn btn-success float-right"> Add categoriess</a>
 </div>
    <div class="card card-default">
        <div class="card-header">
